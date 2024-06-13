46d2dc2ac2f67ac79c99ebbe5e427bf22049c165
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2024-06-13 13:44:07 +0300

    update Quick Start to use runner

--- src/components/ui/runner.tsx
+++ src/components/ui/runner.tsx
@@ -5,14 +5,18 @@ import {
   LivePreview,
 } from 'react-live-runner'
 import * as Reaviz from 'reaviz';
+import * as TopoJsonClient from 'topojson-client';
+import geojson from 'world-atlas/countries-110m.json';
 
 export const Runner = (props) => (
-  <div className="flex w-full border rounded-sm border-gray-800 mt-5">
+  <div className="flex w-full border rounded-sm border-gray-800 light:border-gray-200 mt-5">
     <LiveProvider
       {...props}
       scope={{
         import: {
-          'reaviz': Reaviz
+          'reaviz': Reaviz,
+          'topojson-client': TopoJsonClient,
+          'world-atlas/countries-110m.json': geojson,
         }
       }}
     >
