6c6795a6c42ae96d255373855700f74a46d3714c
Evgeniy <evgeniy@goodcode.us>
2025-07-21 12:31:26 +0300

    Update nav menu font to  --font-inter

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -6,6 +6,7 @@ export const Nav = () => {
   const { resolvedTheme } = useTheme();
 
   return <Navbar
+    className="font-inter"
     logo={
       <div className='flex items-center gap-2'>
         <img src={resolvedTheme === 'light' ? '/logo-light.png' : '/logo.svg'} alt='Reaviz' width={112} height={24} />
